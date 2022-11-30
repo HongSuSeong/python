4bf44d23659c99de0ebbf8da0de779935324f8df
HongSuSeong <hong837198@gmail.com>
2022-11-30 22:36:21 +0900

    입출력& 내장함수

--- 파이썬 기초.py
+++ 파이썬 기초.py
@@ -365,4 +365,35 @@ print("정답은", str(answer), "입니다.")
 answer = 7
 print(f"정답은 {answer}입니다.")
 
+
+# 내장함수
+# 더하기
+result = sum([1,2,3,4,5])
+print(result)
+
+# 작은거
+result = min(7,3,5,2)
+print(result)
+
+
+# 수학 수식이 문자열 형식으로 들어오면 해당 수식을 계산한 결과를 반환
+result = eval("(3+5)*7")
+print(result)
+
+
+# 정렬 오름차순
+result = sorted([9,1,8,5,4])
+print(result)
+
+# 정렬 내림차순
+result = sorted([9,1,8,5,4], reverse = True)
+print(result)
+
+
+# 람다식을 이용한 정렬
+result = sorted([('홍길동',35),('이순신',75),('아무개',50),('강슬기',29)], key = lambda x: x[1], reverse = True)
+print(result)
 """
+data = [9,1,8,5,4]
+data.sort()
+print(data)
